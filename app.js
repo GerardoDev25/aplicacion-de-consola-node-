6db6da5a6b2cf39bc58f16573d451132907d10ec
GerardoDev25 <GerardoDev25@gmail.com>
2021-08-03 16:37:59 -0400

    5. stdin - stdout - Readline

---
+++ app.js
@@ -0,0 +1,12 @@
+require("colors");
+
+const { showMenu, pausa } = require("./helpers/messages");
+
+console.clear();
+
+const main = async (e) => {
+   showMenu();
+// pausa();
+};
+
+main();
---
+++ helpers/messages.js
@@ -0,0 +1,42 @@
+require("colors");
+
+const showMenu = (e) => {
+   console.clear();
+   console.log("=============================".green);
+   console.log("*** seleccione una opcion ***".green);
+   console.log("=============================\n".green);
+
+   console.log(`${"1.".green} Crear tarea`);
+   console.log(`${"2.".green} Listar tareas`);
+   console.log(`${"3.".green} Listar tareas completadas`);
+   console.log(`${"4.".green} Listar tareas pendientes`);
+   console.log(`${"5.".green} Completar tarea(s)`);
+   console.log(`${"6.".green} Borrar Tarea`);
+   console.log(`${"0.".green} Salir\n`);
+
+   const readLine = require("readline").createInterface({
+      input: process.stdin,
+      output: process.stdout,
+   });
+
+   readLine.question("Seleccione una accion: ", (opt) => {
+      readLine.close();
+   });
+};
+
+// ? funcion de pausa de la apliccion
+const pausa = (e) => {
+   const readLine = require("readline").createInterface({
+      input: process.stdin,
+      output: process.stdout,
+   });
+
+   readLine.question(
+      `\nPrecione ${"Enter".green} para continuar\n`,
+      (opt) => {
+         readLine.close();
+      }
+   );
+};
+
+module.exports = { showMenu, pausa };
